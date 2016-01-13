9478cca0cac19c89f19a8874ee74002740494738
vikash11 <ramesh.v@husky.neu.edu>
2016-01-13 16:11:54 -0500

    final

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('/hello', function(req, res){
-   res.send('hello world');
+
+app.use(express.static(__dirname + '/vik'));
+app.get('/vik', function(req, res) {
+    res.send('hellhhho world');
 });
-app.listen(port, ipaddress);
+app.get('/', function(req, res){
+        res.send('main world');
+});
+
+app.listen(port,ipaddress);
